958df5b75d79ffbce15b16ff59f82c9262e3b9e6
coolkeychen <coolkeychen@gmail.com>
2020-10-28 16:31:40 +0800

    update

--- examples/greeter.js
+++ examples/greeter.js
@@ -1,5 +1,14 @@
+var User = /** @class */ (function () {
+    function User(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + ' ' + lastName;
+    }
+    return User;
+}());
+// 类型注解： 明确参数类型及数量
 function greeter(person) {
-    return 'Hello ' + person;
+    return 'Hello ' + person.firstName + ' ' + person.lastName;
 }
-var user = 'Yee';
+var user = new User('chunkai', 'chen');
 console.log(greeter(user));
--- examples/greeter.ts
+++ examples/greeter.ts
@@ -1,8 +1,24 @@
-// 类型注解： 明确参数类型及数量
-function greeter(person: string) {
-  return 'Hello ' + person
+class User {
+  fullName: string
+  firstName: string
+  lastName: string
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullName = firstName + ' ' + lastName
+  }
 }
 
-let user = 'Yee'
+interface Person {
+  firstName: string,
+  lastName: string
+}
+
+// 类型注解： 明确参数类型及数量
+function greeter(person: Person) {
+  return 'Hello ' + person.firstName + ' ' + person.lastName
+}
+
+let user = new User('chunkai','chen')
 
 console.log(greeter(user))
